8094b8aa8c113ad10f07ed9694918360157a4d08
Lovmong <1967471283@qq.com>
2023-05-25 18:10:07 +0800

    optimize get username

--- pironman/app_info.py
+++ pironman/app_info.py
@@ -25,14 +25,15 @@ __version__ = '2.0.0'
 # user_home = os.popen('getent passwd %s | cut -d: -f 6'%username).readline().strip()
 
 # user and User home directory
-abspath = os.path.abspath(__file__)
-statinfo = os.stat(abspath)
-uid = statinfo.st_uid
-gid = statinfo.st_gid
+# abspath = os.path.abspath(__file__)
+# statinfo = os.stat(abspath)
+# uid = statinfo.st_uid
+# gid = statinfo.st_gid
 
-username = pwd.getpwuid(uid)[0]
-group_name = grp.getgrgid(gid)[0]
+# username = pwd.getpwuid(uid)[0]
+# group_name = grp.getgrgid(gid)[0]
 
+username = os.popen("echo ${SUDO_USER:-$(who -m | awk '{ print $1 }')}").readline().strip()
 user_home = os.popen('getent passwd %s | cut -d: -f 6'%username).readline().strip()
 
 config_file = '%s/.config/%s/config.txt'%(user_home, __app_name__)
