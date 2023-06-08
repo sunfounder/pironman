80d9f68bed56063d756f0c5acb9552409f30a2e8
Cavon <cavonxx@gmail.com>
2023-06-08 11:13:17 +0800

    fix api get

--- pironman/ha_api.py
+++ pironman/ha_api.py
@@ -6,26 +6,26 @@ class HomeAssistantSupervisorAPI:
 
     def __init__(self, url, token):
         self.url = url
-        self.token = token
         self.headers = {
             "Authorization": f"Bearer {token}",
             "Content-Type": "application/json",
         }
 
 
-    def request(self, endpoint, data=None):
+    def get(self, endpoint):
         try:
             url = f"{self.url}/{endpoint}"
-            r = requests.post(url, headers=self.headers)
-            log(msg="home assistant request: " + r.text, level='DEBUG')
+            log(msg=f"home assistant get: {url}", level='DEBUG')
+            r = requests.get(url, headers=self.headers)
+            log(msg="home assistant got: " + r.text, level='DEBUG')
             return r.json()
         except Exception as e:
-            log(msg="home assistant request error: " + e, level='DEBUG')
+            log(msg="home assistant get error: " + e, level='DEBUG')
 
 
     def get_ip(self):
         IPs = {}
-        data = self.request("network/info")
+        data = self.get("network/info")
         interfaces = data["data"]["interfaces"]
         for interface in interfaces:
             name = interface['interface']
@@ -36,5 +36,5 @@ class HomeAssistantSupervisorAPI:
     def shutdown(self):
         '''shutdown homeassistant host'''
         log(msg="Shutdown home assistant host", level='DEBUG')
-        self.request("host/shutdown")
+        self.get("host/shutdown")
 
