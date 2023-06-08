6a9acc8610099fbec41675f453e9356962d14a20
Cavon <cavonxx@gmail.com>
2023-06-08 12:01:47 +0800

    remove log

--- pironman/ha_api.py
+++ pironman/ha_api.py
@@ -15,9 +15,7 @@ class HomeAssistantSupervisorAPI:
     def get(self, endpoint):
         try:
             url = f"{self.url}{endpoint}"
-            log(msg=f"home assistant get: {url}", level='DEBUG')
             r = requests.get(url, headers=self.headers)
-            log(msg="home assistant got: " + r.text, level='DEBUG')
             return r.json()
         except Exception as e:
             log(msg="home assistant get error: " + e, level='DEBUG')
